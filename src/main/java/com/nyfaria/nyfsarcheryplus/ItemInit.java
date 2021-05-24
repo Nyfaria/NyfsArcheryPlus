fc9df50c25fa50a99eec4bccd62e8b4a4461598c
Nyfaria <kinaria3324@gmail.com>
2021-05-24 14:53:32 -0500

    final revision before release

--- src/main/java/com/nyfaria/nyfsarcheryplus/ItemInit.java
+++ src/main/java/com/nyfaria/nyfsarcheryplus/ItemInit.java
@@ -37,25 +37,32 @@ public class ItemInit {
 	
 	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, NyfsArcheryPlus.MOD_ID);
 
-    public static final StoneTippedArrowItem STONE_TIPPED_ARROW_ITEM = new StoneTippedArrowItem(new Item.Properties().stacksTo(64).tab(ItemGroup.TAB_COMBAT));
-    public static final GoldTippedArrowItem GOLD_TIPPED_ARROW_ITEM = new GoldTippedArrowItem(new Item.Properties().stacksTo(64).tab(ItemGroup.TAB_COMBAT));
-    public static final IronTippedArrowItem IRON_TIPPED_ARROW_ITEM = new IronTippedArrowItem(new Item.Properties().stacksTo(64).tab(ItemGroup.TAB_COMBAT));
-    public static final DiamondTippedArrowItem DIAMOND_TIPPED_ARROW_ITEM = new DiamondTippedArrowItem(new Item.Properties().stacksTo(64).tab(ItemGroup.TAB_COMBAT));
-    public static final NetheriteTippedArrowItem NETHERITE_TIPPED_ARROW_ITEM = new NetheriteTippedArrowItem(new Item.Properties().stacksTo(64).tab(ItemGroup.TAB_COMBAT));
+    
 	public static final RegistryObject<IronBowItem> IRON_BOW = ITEMS.register("iron_bow", () -> new IronBowItem(new Item.Properties()));
 	public static final RegistryObject<GoldBowItem> GOLD_BOW = ITEMS.register("gold_bow", () -> new GoldBowItem(new Item.Properties()));
 	public static final RegistryObject<DiamondBowItem> DIAMOND_BOW = ITEMS.register("diamond_bow", () -> new DiamondBowItem(new Item.Properties()));
 	public static final RegistryObject<NetheriteBowItem> NETHERITE_BOW = ITEMS.register("netherite_bow", () -> new NetheriteBowItem(new Item.Properties()));
+
+    public static final StoneTippedArrowItem STONE_TIPPED_ARROW_ITEM = new StoneTippedArrowItem(new Item.Properties().stacksTo(64).tab(ItemGroup.TAB_COMBAT));
 	public static final RegistryObject<StoneArrowTipItem> STONE_ARROW_TIP = ITEMS.register("stone_arrow_tip", () -> new StoneArrowTipItem(new Item.Properties().stacksTo(64).tab(ItemGroup.TAB_COMBAT)));
 	public static final RegistryObject<StoneTippedArrowItem> STONE_TIPPED_ARROW = ITEMS.register("stone_tipped_arrow", () -> STONE_TIPPED_ARROW_ITEM);
+
+    public static final GoldTippedArrowItem GOLD_TIPPED_ARROW_ITEM = new GoldTippedArrowItem(new Item.Properties().stacksTo(64).tab(ItemGroup.TAB_COMBAT));
 	public static final RegistryObject<GoldArrowTipItem> GOLD_ARROW_TIP = ITEMS.register("gold_arrow_tip", () -> new GoldArrowTipItem(new Item.Properties().stacksTo(64).tab(ItemGroup.TAB_COMBAT)));
 	public static final RegistryObject<GoldTippedArrowItem> GOLD_TIPPED_ARROW = ITEMS.register("gold_tipped_arrow", () -> GOLD_TIPPED_ARROW_ITEM);
+
+    public static final IronTippedArrowItem IRON_TIPPED_ARROW_ITEM = new IronTippedArrowItem(new Item.Properties().stacksTo(64).tab(ItemGroup.TAB_COMBAT));
 	public static final RegistryObject<IronArrowTipItem> IRON_ARROW_TIP = ITEMS.register("iron_arrow_tip", () -> new IronArrowTipItem(new Item.Properties().stacksTo(64).tab(ItemGroup.TAB_COMBAT)));
 	public static final RegistryObject<IronTippedArrowItem> IRON_TIPPED_ARROW = ITEMS.register("iron_tipped_arrow", () -> IRON_TIPPED_ARROW_ITEM);
+
+    public static final DiamondTippedArrowItem DIAMOND_TIPPED_ARROW_ITEM = new DiamondTippedArrowItem(new Item.Properties().stacksTo(64).tab(ItemGroup.TAB_COMBAT));
 	public static final RegistryObject<DiamondArrowTipItem> DIAMOND_ARROW_TIP = ITEMS.register("diamond_arrow_tip", () -> new DiamondArrowTipItem(new Item.Properties().stacksTo(64).tab(ItemGroup.TAB_COMBAT)));
 	public static final RegistryObject<DiamondTippedArrowItem> DIAMOND_TIPPED_ARROW = ITEMS.register("diamond_tipped_arrow", () -> DIAMOND_TIPPED_ARROW_ITEM);
+
+    public static final NetheriteTippedArrowItem NETHERITE_TIPPED_ARROW_ITEM = new NetheriteTippedArrowItem(new Item.Properties().stacksTo(64).tab(ItemGroup.TAB_COMBAT));
 	public static final RegistryObject<NetheriteArrowTipItem> NETHERITE_ARROW_TIP = ITEMS.register("netherite_arrow_tip", () -> new NetheriteArrowTipItem(new Item.Properties().stacksTo(64).tab(ItemGroup.TAB_COMBAT)));
 	public static final RegistryObject<NetheriteTippedArrowItem> NETHERITE_TIPPED_ARROW = ITEMS.register("netherite_tipped_arrow", () -> NETHERITE_TIPPED_ARROW_ITEM);
+	
 	
 	
 	public static void preInit() {
--- src/main/java/com/nyfaria/nyfsarcheryplus/items/DiamondBowItem.java
+++ src/main/java/com/nyfaria/nyfsarcheryplus/items/DiamondBowItem.java
@@ -2,10 +2,20 @@ package com.nyfaria.nyfsarcheryplus.items;
 
 import net.minecraft.item.BowItem;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.item.ArmorMaterial;
 
 public class DiamondBowItem extends BowItem {
     
 	public DiamondBowItem(Properties properties) {
 		super(new Properties().stacksTo(1).defaultDurability(2534).tab(ItemGroup.TAB_COMBAT));
+	}
+	@Override
+	public boolean isValidRepairItem(ItemStack p_82789_1_, ItemStack p_82789_2_) {
+		if(p_82789_2_.getItem() == Items.DIAMOND_SWORD) {
+			return true;
+		}
+		return false;
 	}
 }
