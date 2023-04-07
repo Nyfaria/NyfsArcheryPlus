0444a1f0d6c29ee3160b34e36a574d428d8b509f
Nyfaria DragonMoose <kinaria3324@gmail.com>
2023-04-06 22:08:11 -0500

    push
    Took 26 minutes

--- Fabric/src/main/java/com/nyfaria/nyfsarcheryplus/platform/FabricPlatformHelper.java
+++ Fabric/src/main/java/com/nyfaria/nyfsarcheryplus/platform/FabricPlatformHelper.java
@@ -1,7 +1,12 @@
 package com.nyfaria.nyfsarcheryplus.platform;
 
+import com.nyfaria.nyfsarcheryplus.entity.AdvancedTippedArrowEntity;
+import com.nyfaria.nyfsarcheryplus.enums.ArcheryTiers;
+import com.nyfaria.nyfsarcheryplus.interfaces.IArcherCollection;
 import com.nyfaria.nyfsarcheryplus.platform.services.IPlatformHelper;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.item.Item;
 
 public class FabricPlatformHelper implements IPlatformHelper {
 
@@ -20,5 +25,20 @@ public class FabricPlatformHelper implements IPlatformHelper {
     public boolean isDevelopmentEnvironment() {
 
         return FabricLoader.getInstance().isDevelopmentEnvironment();
+    }
+
+    @Override
+    public EntityType<AdvancedTippedArrowEntity> getAdvancedTippedArrowEntity() {
+        return null;
+    }
+
+    @Override
+    public Item getCommonTippedArrow(ArcheryTiers tier) {
+        return null;
+    }
+
+    @Override
+    public IArcherCollection getArcherCollection(ArcheryTiers tier) {
+        return null;
     }
 }
